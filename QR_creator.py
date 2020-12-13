65e51eab37adb0953df61e1f53453595131f5a01
WoutDavid <wouters.david@hotmail.com>
2020-12-13 12:24:06 +0100

    added QR functionality and QRcodes

--- QR_creator.py
+++ QR_creator.py
@@ -1,2 +1,29 @@
 import qrcode
+import os
 
+# directory="/home/david/Documents/WoutDavid.github.io/html"
+# for file in os.listdir(directory):
+#     if file.endswith("html"):
+#         url = directory +"/" + file
+#         without_extension=os.path.splitext(file)
+#         qr = qrcode.QRCode(
+#                 version=1,
+#                 box_size=10,
+#                 border=5)
+#         qr.add_data(url)
+#         qr.make(fit=True)
+
+#         img = qr.make_image(fill='black', back_color='white')
+#         img.save("/home/david/Documents/WoutDavid.github.io/QRcodes/" + "QR_" + without_extension[0])
+
+
+url = "/home/david/Documents/WoutDavid.github.io/index.html"
+qr = qrcode.QRCode(
+                version=1,
+                box_size=10,
+                border=5)
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+img.save("/home/david/Documents/WoutDavid.github.io/QRcodes/" + "QR_index")
